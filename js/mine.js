48634162604944e3833abf65c31d693e9a3e0fb7
Elattar11 <mohamed.ahmed7350@fmail.com>
2023-08-21 14:24:37 +0300

    initial commit

---
+++ js/mine.js
@@ -0,0 +1,52 @@
+$(".open-icon").click(function(){
+    $(".menu").css("left" , "0");
+    $("#home-content").animate({marginLeft :'250px'},50);
+})
+
+$(".closeIcn").click(function(){
+    $(".menu").css("left" , "-250px");
+    $("#home-content").animate({marginLeft :'0px'},50);
+})
+
+$(".component h3").click(function(){
+
+    $(this).next().slideToggle();
+    $(".component .component-about").not($(this).next()).slideUp();
+})
+
+window.onload = function() {
+   
+    countDownToTime("10 october 2021 9:56:00");
+}
+
+function countDownToTime(countTo) {
+  
+    let futureDate = new Date(countTo);
+futureDate = (futureDate.getTime()/1000);
+
+let now = new Date();
+now = (now.getTime()/1000);
+
+timeDifference = (futureDate- now);
+    
+let days = Math.floor( timeDifference / (24*60*60));
+let hours = Math.floor((timeDifference - (days * (24*60*60))) / 3600);
+let mins = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600 )) / 60);
+let secs = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600) - (mins * 60)))
+
+
+$(".days").html(`${days} D`);
+$(".hours").html(`${hours} h`);
+$(".minutes").html(`${ mins } m`);
+$('.seconds').html(`${ secs} s`)
+
+
+setInterval(function() {  countDownToTime(countTo); }, 1000);
+}
+
+$("textarea").keyup(function(){
+    let charsLength = $(this).val().length;
+    $("#chars-num").text(
+        100 - charsLength <= 0 ? "Available charcters finished " : 100 - charsLength+" "
+    )
+})
